a690c7813a86f7ae4bea97e7e29944b8c0a5254c
Matteo <matteonegridev@gmail.com>
2024-08-03 20:38:46 +0200

    added id as key

--- src/Components/SoundButtons.jsx
+++ src/Components/SoundButtons.jsx
@@ -1,23 +1,27 @@
 const soundsColor = [
   {
+    id: 0,
     name: "Suikinkutsu",
     color: "#daff6f",
     description: "Water Drip Resonance",
     sound: new Audio("assets/Suikinkutsu.mp3"),
   },
   {
+    id: 1,
     name: "Cicadas",
     color: "#A8AEEF",
     description: "Insect Chorus",
     sound: new Audio("./assets/Cicadas.mp3"),
   },
   {
+    id: 2,
     name: "Temple Bells",
     color: "#A8AEEF",
     description: "Waterfall Roar",
     sound: new Audio("./assets/TempleBellSound.mp3"),
   },
   {
+    id: 3,
     name: "Shomyo Falls",
     color: "#daff6f",
     description: "Bell Resonance",
@@ -35,17 +39,14 @@ function Buttons() {
   return (
     <>
       {/* Destructuring per rilevare le properties dall'oggetto */}
-      {soundsColor.map(({ name, color, description, sound }, index) => (
+      {soundsColor.map(({ name, color, description, sound, id }, index) => (
         <button
           id={`btn-${index}`}
           className="p-20 relative rounded-[10px] active:scale-[0.98] transition-all 150ms ease-in "
           style={{ backgroundColor: color }}
-          key={index}
+          key={id}
           onClick={() => playSound(sound)}
         >
-          <audio id={name} className="sound">
-            <source src={sound} type="audio/mp3"></source>
-          </audio>
           <div className="absolute top-20 w-3/4 left-3 text-left">
             <p className="text-[#000] font-bold font-sourceSans">{name}</p>
             <small className="font-sourceSans text-secondSubtext font-light ">
