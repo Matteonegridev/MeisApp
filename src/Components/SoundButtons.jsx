0af830f300e72a457163a491d9f78cf62c35cffe
Matteo <matteonegridev@gmail.com>
2024-08-04 20:18:16 +0200

    implemented logic for disabling sound when playing another + return to 0

--- src/Components/SoundButtons.jsx
+++ src/Components/SoundButtons.jsx
@@ -1,11 +1,26 @@
-function playSound(soundUrl) {
-  soundUrl.play();
-  console.log("Playing sound:", soundUrl);
-}
-
-//TODO Install React Player
+import PropTypes from "prop-types";
+import { useState, useRef } from "react";
+import ReactPlayer from "react-player";
 
 function Buttons({ soundsColor }) {
+  const [alternativeText, setAlternativeText] = useState(null);
+  const [playing, setPlaying] = useState(false);
+  const [currentSound, setCurrentSound] = useState(null);
+  const playerRef = useRef(null);
+
+  function handleText(id) {
+    setAlternativeText(id);
+  }
+
+  function handleSound(soundUrl) {
+    if (currentSound && currentSound !== soundUrl) {
+      playerRef.current.seekTo(0);
+      setPlaying(false);
+    }
+    setCurrentSound(soundUrl);
+    setPlaying(true);
+  }
+
   return (
     <>
       {/* Destructuring per rilevare le properties dall'oggetto */}
@@ -15,12 +30,22 @@ function Buttons({ soundsColor }) {
           className="p-20 relative rounded-[10px] active:scale-[0.98] transition-all 150ms ease-in "
           style={{ backgroundColor: color }}
           key={id}
-          onClick={() => playSound(sound)}
+          onClick={() => {
+            handleSound(sound);
+            handleText(id);
+          }}
         >
+          <ReactPlayer
+            ref={playerRef}
+            url={currentSound}
+            playing={playing && currentSound === sound}
+            width="100%"
+            height="100%"
+          />
           <div className="absolute top-20 w-3/4 left-3 text-left">
             <p className="text-[#000] font-bold font-sourceSans">{name}</p>
             <small className="font-sourceSans text-secondSubtext font-light ">
-              {description}
+              {alternativeText === id ? "Playing..." : description}
             </small>
           </div>
         </button>
@@ -29,6 +54,18 @@ function Buttons({ soundsColor }) {
   );
 }
 
+Buttons.propTypes = {
+  soundsColor: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      description: PropTypes.string,
+      color: PropTypes.string,
+      sound: PropTypes.string,
+    })
+  ),
+};
+
 export function SoundScapes() {
   const soundsColor = [
     {
@@ -36,28 +73,28 @@ export function SoundScapes() {
       name: "Suikinkutsu",
       color: "#daff6f",
       description: "Water Drip Resonance",
-      sound: new Audio("assets/Suikinkutsu.mp3"),
+      sound: "./assets/Suikinkutsu.mp3",
     },
     {
       id: 1,
       name: "Cicadas",
       color: "#A8AEEF",
       description: "Insect Chorus",
-      sound: new Audio("./assets/Cicadas.mp3"),
+      sound: "./assets/Cicadas.mp3",
     },
     {
       id: 2,
       name: "Temple Bells",
       color: "#A8AEEF",
       description: "Waterfall Roar",
-      sound: new Audio("./assets/TempleBellSound.mp3"),
+      sound: "./assets/TempleBellSound.mp3",
     },
     {
       id: 3,
       name: "Shomyo Falls",
       color: "#daff6f",
       description: "Bell Resonance",
-      sound: new Audio("./assets/Waterfall.mp3"),
+      sound: "./assets/Waterfall.mp3",
     },
   ];
 
