fca5a1fa7a669568a899f5d9165a0e4da993fe2a
Matteo <matteonegridev@gmail.com>
2024-08-17 13:26:24 +0200

    created buttons that will change the timer

--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -1,16 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { SoundScapes } from "./SoundButtons";
+import ReactPlayer from "react-player";
 
-export function Timer() {
+const timers = [
+  { time: 10, id: 1 },
+  { time: 15, id: 2 },
+  { time: 20, id: 3 },
+];
+
+function Timer() {
   const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(25);
+  const [minutes, setMinutes] = useState(10);
+
+  function handleTime(time) {
+    setMinutes(time);
+  }
 
   return (
-    <div className="flex justify-center items-center flex-col mt-2 font-sourceSans">
-      <h2 className="text-white text-5xl font-bold p-1">Timer</h2>
-      <div>
+    <div className="mt-2 font-sourceSans">
+      <h2 className="text-white text-2xl font-semibold p-1 text-center">
+        How long you want to relax for?
+      </h2>
+      <div className="p-3 flex justify-around">
+        {timers.map(({ time, id }) => {
+          return (
+            <button
+              onClick={() => handleTime(time)}
+              className="bg-primary text-black px-7 py-1 font-sourceSans font-bold rounded-md text-2xl"
+              key={id}
+            >
+              {time}
+            </button>
+          );
+        })}
+      </div>
+      <div className="mt-3 text-center font-semibold">
         <span className="text-white text-4xl">{minutes}:</span>
-        <span className="text-white text-4xl">{seconds}</span>
+        <span className="text-white text-4xl">00</span>
       </div>
     </div>
   );
+}
+
+export function Time() {
+  return <Timer />;
 }
--- src/sound.jsx
+++ src/sound.jsx
@@ -4,11 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import { SoundScapes } from "./Components/SoundButtons.jsx";
 import { Header } from "./Components/SoundHeader.jsx";
+import { Time } from "./Components/Timer.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Header />
     <SoundScapes />
+    <Time />
   </React.StrictMode>
 );
