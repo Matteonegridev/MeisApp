ff839ac51c57ad343b37e9554275a0c3c91ec00b
Matteo <matteonegridev@gmail.com>
2024-08-21 00:12:05 +0200

    created buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
+import { SoundScapes } from "./Components/SoundButtons";
 
 function App() {
   const [text, setText] = useState(null);
   const [playing, setPlaying] = useState(false);
   const [currentSound, setCurrentSound] = useState(null);
 
-  return <></>;
+  return (
+    <>
+      <SoundScapes text={text} />
+    </>
+  );
 }
 
 export default App;
--- src/Components/SoundButtons.jsx
+++ src/Components/SoundButtons.jsx
@@ -2,35 +2,55 @@ import PropTypes from "prop-types";
 import { useState, useRef } from "react";
 import ReactPlayer from "react-player";
 
-const soundsColor = [
-  {
-    id: 0,
-    name: "Suikinkutsu",
-    color: "#daff6f",
-    description: "Water Drip Resonance",
-    sound: "./assets/Suikinkutsu.mp3",
-  },
-  {
-    id: 1,
-    name: "Cicadas",
-    color: "#A8AEEF",
-    description: "Insect Chorus",
-    sound: "./assets/Cicadas.mp3",
-  },
-  {
-    id: 2,
-    name: "Temple Bells",
-    color: "#A8AEEF",
-    description: "Waterfall Roar",
-    sound: "./assets/TempleBellSound.mp3",
-  },
-  {
-    id: 3,
-    name: "Shomyo Falls",
-    color: "#daff6f",
-    description: "Bell Resonance",
-    sound: "./assets/Waterfall.mp3",
-  },
-];
+function Buttons({ soundsColor }) {
+  return (
+    <>
+      {soundsColor.map(({ id, name, color, description, sound }, index) => {
+        <button
+          id={`btn-${index}`}
+          className="p-20 relative rounded-[10px] active:scale-[0.98] transition-all 150ms ease-in "
+          key={id}
+          name={name}
+          color={{ backgroundColor: color }}
+        >
+          {description}
+        </button>;
+      })}
+    </>
+  );
+}
 
-function Buttons({ text, setText }) {}
+export function SoundScapes() {
+  const soundsColor = [
+    {
+      id: 0,
+      name: "Suikinkutsu",
+      color: "#daff6f",
+      description: "Water Drip Resonance",
+      sound: "./assets/Suikinkutsu.mp3",
+    },
+    {
+      id: 1,
+      name: "Cicadas",
+      color: "#A8AEEF",
+      description: "Insect Chorus",
+      sound: "./assets/Cicadas.mp3",
+    },
+    {
+      id: 2,
+      name: "Temple Bells",
+      color: "#A8AEEF",
+      description: "Waterfall Roar",
+      sound: "./assets/TempleBellSound.mp3",
+    },
+    {
+      id: 3,
+      name: "Shomyo Falls",
+      color: "#daff6f",
+      description: "Bell Resonance",
+      sound: "./assets/Waterfall.mp3",
+    },
+  ];
+
+  return <Buttons soundsColor={soundsColor} />;
+}
