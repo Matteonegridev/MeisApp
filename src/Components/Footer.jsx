924d1cf5938381f5c9b54d9927b493d97f9529ec
Matteo <matteonegridev@gmail.com>
2024-09-04 12:38:58 +0200

    styled main page

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="xsm:hidden absolute bottom-2 left-10 md:z-10 ">
+      <small className="text-gray-400 relative">
+        &copy; Matteo Negri - MeisApp 2024{" "}
+      </small>
+    </footer>
+  );
+}
--- src/Components/Page.jsx
+++ src/Components/Page.jsx
@@ -14,8 +14,8 @@ const animatePage = (event) => {
 
 export function MainPage() {
   return (
-    <main className="flex flex-col md:min-h-screen md:bg-hero-pattern md:bg-top md:bg-cover md:bg-overlay md:relative ">
-      <div className="w-4/5 m-auto p-5 text-center mt-9 md:w-full md:text-pretty md:relative md:z-10">
+    <main className=" flex flex-col items-center  md:min-h-screen md:bg-hero-pattern md:bg-top md:bg-cover md:bg-overlay md:relative ">
+      <div className="xsm:pt-16 w-4/5 m-auto p-5 text-center mt-9 md:w-full md:text-pretty md:relative md:z-10">
         <h1 className="text-white text-7xl font-playfair md:text-[10rem] md:pt-20 md:tracking-widest">
           Meditate
           <br />
@@ -23,26 +23,28 @@ export function MainPage() {
         </h1>
       </div>
       <div className="xsm:hidden w-[80%] m-auto md:relative md:z-10">
-        <p className="text-white font-sourceSans text-2xl">
-          Well rested, better productivity.
+        <p className="text-white font-sourceSans text-2xl font-thin">
+          <em> Well rested, better productivity.</em>
         </p>
       </div>
       <figure className="md:hidden">
         <img
-          className="mt-10 block w-[90%] m-auto  "
+          className="mt-10 block w-full m-auto  "
           src="./img/meditation.svg"
         ></img>
       </figure>
-      <a
-        id="btn"
-        onClick={animatePage}
-        href="main.html"
-        className=" mt-24 self-center px-14 py-3 rounded-md bg-primary md:relative md:z-10"
-      >
-        <span className="font-sourceSans text-[#000] font-semibold text-4xl">
-          Start
-        </span>
-      </a>
+      <div className="md:p-10 md:relative md:z-10 xsm:mt-24">
+        <a
+          id="btn"
+          onClick={animatePage}
+          href="main.html"
+          className="px-14 py-3 rounded-xl bg-primary  flex justify-center items-center"
+        >
+          <span className="font-sourceSans text-[#000] font-semibold text-3xl xsm:text-4xl">
+            Start
+          </span>
+        </a>
+      </div>
     </main>
   );
 }
