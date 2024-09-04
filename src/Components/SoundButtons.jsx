5c0a02044ec8f8093b7123c91cc4ac63951b8a93
Matteo <matteonegridev@gmail.com>
2024-09-04 18:50:04 +0200

    changed path to files mp3

--- src/Components/SoundButtons.jsx
+++ src/Components/SoundButtons.jsx
@@ -6,28 +6,28 @@ export const soundsColor = [
     name: "Suikinkutsu",
     color: "#daff6f",
     description: "Water Drip Resonance",
-    sound: "./assets/Suikinkutsu.mp3",
+    sound: "./dist/assets/Suikinkutsu.mp3",
   },
   {
     id: 1,
     name: "Cicadas",
     color: "#A8AEEF",
     description: "Insect Chorus",
-    sound: "./assets/Cicadas.mp3",
+    sound: "./dist/assets/Cicadas.mp3",
   },
   {
     id: 2,
     name: "Temple Bells",
     color: "#A8AEEF",
     description: "Waterfall Roar",
-    sound: "./assets/TempleBellSound.mp3",
+    sound: "./dist/assets/TempleBellSound.mp3",
   },
   {
     id: 3,
     name: "Shomyo Falls",
     color: "#daff6f",
     description: "Bell Resonance",
-    sound: "./assets/Waterfall.mp3",
+    sound: "./dist/assets/Waterfall.mp3",
   },
 ];
 
